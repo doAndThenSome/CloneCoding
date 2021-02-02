7951c00413092ba2a10d94d392ca6671fc599533
doAndThenSome <do.andthensome@gmail.com>
2021-02-02 22:50:01 +0900

    My first clone iOS app UpDownGame

---
+++ UpDownGame/UpDownGame/CreditViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 오은지 on 2020/11/22.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
